6a6aee5b36aa885ecf4ace98bbae8ef672e2f046
Mandizvidzafn <ncebamandizvidza@gmail.com>
2023-02-09 11:12:22 -0800

    python classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""square class to represent a square"""
+
+class Sqaure():
+    """Empty class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""setting up attributes"""
+
+class Square():
+    """representation of a square class"""
+    
+    def __init__(self, size):
+        """ initialize the class
+
+        Args:
+            size (int): size of the square"""
+        self.__size = size

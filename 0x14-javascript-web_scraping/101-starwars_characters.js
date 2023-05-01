17e79f008ece461afea606b34460bb14e36d1913
Mandizvidzafn <ncebamandizvidza@gmail.com>
2023-05-02 01:25:40 +0200

    using the module request in file 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fetch = require('node-fetch');
+const request = require('request');
 
 // Get the movie ID from the command-line argument
 if (process.argv.length < 3) {
@@ -10,26 +10,26 @@ const movieId = process.argv[2];
 
 // Send a GET request to the Star Wars API
 const url = `https://swapi.dev/api/films/${movieId}/`;
-fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error}`);
+  } else if (response.statusCode !== 200) {
+    console.error(`HTTP error! status: ${response.statusCode}`);
+  } else {
+    const data = JSON.parse(body);
     // Get the character URLs from the response and send a GET request for each character
     const characterUrls = data.characters;
-    return Promise.all(characterUrls.map(url => fetch(url)));
-  })
-  .then(responses => {
-    // Print the character names from the responses
-    for (const response of responses) {
-      if (response.ok) {
-        response.json().then(data => console.log(data.name));
-      }
-    }
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    characterUrls.forEach(characterUrl => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error(`Error: ${error}`);
+        } else if (response.statusCode !== 200) {
+          console.error(`HTTP error! status: ${response.statusCode}`);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  }
+});
